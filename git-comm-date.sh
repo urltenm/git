49e54d82db12cff29b173ff9efe0979a1548a6a9
Max T <tenm.ccnp@gmail.com>
2025-12-05 20:59:16 +0500

    Date:05.12.2025 Time:08:59:16 Who:kritik

--- git-comm-date.sh
+++ git-comm-date.sh
@@ -1,10 +1,12 @@
 git-md() {
     local formatted_date=$(date "+%Y-%m-%d %H:%M:%S")
     if [ $# -eq 0 ]; then
-        git commit -m ""Date:"$(date +'%d.%m.%Y') "Time:"$(date | grep -oP "(([0-9]{2}:){2}[0-9]{2})") "Who:$(whoami)""
-    else
-        git commit -m "$*" "Date:"$(date +'%d.%m.%Y') "Time:"$(date | grep -oP "(([0-9]{2}:){2}[0-9]{2})") "Who:$(whoami)""
+        #git commit -m "Date:$(date +'%d.%m.%Y')" "Time:$(date)" | grep -oP "(([0-9]{2}:){2}[0-9]{2})") "Who:$(whoami)"
+	git commit -m "Date:$(date +'%d.%m.%Y')"
 
+    else
+        #git commit -m "$*" "Date:"$(date +'%d.%m.%Y') "Time:"$(date | grep -oP "(([0-9]{2}:){2}[0-9]{2})") "Who:$(whoami)"
+	git commit -m "$*" "Date:$(date +'%d.%m.%Y')"
 
     fi
 }
